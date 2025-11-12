15c37bd607b7288b1190f9cb7cb15b9f4a9ad8b1
Erica Stefny Ferrao <ferraoerica@gmail.com>
2025-11-12 06:08:27 -0800

    create ticket reducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import './styles.css';
 import TicketForm from './components/TicketForm';
+import ticketReducer from './reducers/ticketReducer';
+import { useReducer } from 'react';
 
 function App() {
+  const initialState = { tickets: [] };
+
+  const [state, dispatch] = useReducer(ticketReducer, initialState);
   return (
     <div className="App">
       <div className='container'>
         <h1>Bug Blaster</h1>
-        <TicketForm />
+        <TicketForm dispatch={dispatch} />
       </div>
     </div>
   );
